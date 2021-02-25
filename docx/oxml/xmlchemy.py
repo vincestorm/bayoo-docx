711b54c3aaeb6091ae118f01d64720a3d3a6914e
Tareq Ibrahim <tareq.ibrahim@l-one.de>
2021-02-25 15:06:48 +0200

    fix for debuggers not showing content of objects when running python 3.8+

--- docx/oxml/xmlchemy.py
+++ docx/oxml/xmlchemy.py
@@ -752,7 +752,10 @@ class _OxmlElementBase(etree.ElementBase):
     @property
     def _nsptag(self):
         return NamespacePrefixedTag.from_clark_name(self.tag)
-
+    
+    def __dict__(self):
+        # __dict__ is required for debuggers to show the content of objects when running Python 3.8+
+        return None
 
 BaseOxmlElement = MetaOxmlElement(
     'BaseOxmlElement', (etree.ElementBase,), dict(_OxmlElementBase.__dict__)
